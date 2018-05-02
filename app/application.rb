de7c574f11c2110af1108d2fdd59d34a99e466b6
Michelle Olivieri <michelleo72@gmail.com>
2018-05-02 00:11:43 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -6,6 +6,12 @@ class Application
     resp = Rack::Response.new
     req = Rack::Request.new(env)
 
+    if req.path.match(/items/)
+      item_name = req.path.split("/items/").last
+      item = @@items.find{|i| i.name == item_name}
+      resp.write item.price
+    end
+
   end
   resp.finish
 end
