bbb322635e37a15efc3a2fa3f99a418c2ff66b77
Michelle Olivieri <michelleo72@gmail.com>
2018-05-02 00:24:31 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -10,7 +10,7 @@ class Application
       item_name = req.path.split("/items/").last
       item = @@items.find{|i| i.name == item_name}
       resp.write item.price if item
-      resp.status = 400
+      #resp.status = 400
 
     end
     else
