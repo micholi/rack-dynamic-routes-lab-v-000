2e7e94acce486469165aad9b3a92781e05fb8a9e
Michelle Olivieri <michelleo72@gmail.com>
2018-05-02 00:26:51 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -9,12 +9,12 @@ class Application
     if req.path.match(/items/)
       item_name = req.path.split("/items/").last
       item = @@items.find{|i| i.name == item_name}
-      resp.write item.price 
+      resp.write item.price
       #resp.status = 400
 
     end
-    else
-      resp.status = 404
-      resp.finish
+    #else
+    #  resp.status = 404
+    #  resp.finish
   end
 end
